a05df11ffb73220f372d98eca1fb685f9ac29079
Coleton Watt <electricninja102@gmail.com>
2022-02-14 22:39:19 -0700

    Initial Commit

---
+++ No Brainer: Recursion Maze 6.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No Brainer: Recursion Maze 6.1
+//
+//  Created by Coleton Watt on 2/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
