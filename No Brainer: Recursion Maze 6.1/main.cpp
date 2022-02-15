3e70bf3ba081d21fc76dff80359bb3d22da9ae8e
Coleton Watt <electricninja102@gmail.com>
2022-02-15 10:55:43 -0700

    No brainer assignment complete

--- No Brainer: Recursion Maze 6.1/main.cpp
+++ No Brainer: Recursion Maze 6.1/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by Coleton Watt on 2/14/22.
 //
-
+/*
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
@@ -12,3 +12,4 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+*/
